5d87b1d479b85ecff8478cd886b0ca2c6c24837e
PEdroALcazar84 <pedro.alcazar84@gmail.com>
2022-09-25 19:24:57 +0200

    Exercise4

--- projects/exercise4/src/app/app.component.spec.ts
+++ projects/exercise4/src/app/app.component.spec.ts
@@ -1,14 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule
+      ],
+      declarations: [
+        AppComponent
+      ]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+/* This is a test that checks if the app is created. */
+  it('Exercise 4 - Should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
--- projects/exercise4/src/app/app.component.ts
+++ projects/exercise4/src/app/app.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+/* The AppComponent class is a TypeScript class that has a title property that is a string and is set to "Exercise 4" */
+export class AppComponent {
+
+  title: string = 'Exercise 4';
+  header$: string = "Header";
+  sidebar$: string = "Sidebar";
+  feature$: string = "Feature";
+  a$: string = "A";
+  b$: string = "B";
+
+}
