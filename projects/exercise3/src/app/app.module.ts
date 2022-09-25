bd1fee7e1194f8c54b994f727e189a78ed5325cb
PEdroALcazar84 <pedro.alcazar84@gmail.com>
2022-09-25 19:22:42 +0200

    Exercise3

--- projects/exercise3/src/app/app.module.ts
+++ projects/exercise3/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- projects/exercise3/src/app/data.service.ts
+++ projects/exercise3/src/app/data.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable} from '@angular/core';
+import { Observable,interval, map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
+/* The DataService class is a class that has a constructor that takes no parameters. */
 export class DataService {
+  constructor() { }
+
+/**
+ * It returns an Observable that emits a random number every second
+ * @returns An Observable that emits a random number every second.
+ */
   getNumbers(): Observable<number> {
-    throw new Error('Method not implemented');
+    try {
+      throw new Error('Method not implemented');
+    } catch (error) {
+      return interval(1000).pipe(map(() => Math.floor(Math.random() * (10 - 1) + 1)));
+    }
   }
 }
